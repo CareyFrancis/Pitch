4c7daee6889f58d1700e4bc11bcc6ba24d2ce7b2
nimomaina <nimomaina.nm@gmail.com>
2019-02-10 17:50:59 +0300

    Create a database and add cofigurations

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wairimu:touce@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
