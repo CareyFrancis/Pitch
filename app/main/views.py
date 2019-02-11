6406cf7580750cbcfe166a3375ba6e39f10f1038
nimomaina <nimomaina.nm@gmail.com>
2019-02-11 23:06:29 +0300

    add comment and vote forms

--- app/main/views.py
+++ app/main/views.py
@@ -1,61 +1,106 @@
-# from flask import render_template,request,redirect,url_for
-# from . import main
-# from .forms import PitchForm
-# from ..models import Pitch
-# from flask_login import login_required
-#
-# # Views
-# @main.route('/')
-# def index():
-#
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     title  = 'Pitch - One Minute Chance to Pitch'
-#     search_pitch = request.args.get('pitch_query')
-#     pitches = Pitch.get_pitches()
-#
-#
-#     return render_template('index.html', title = title, pitches = pitches)
-#
-# #
-# # @main.route('/pick-up/pitches/')
-# # def pick_up():
-# #
-# #     '''
-# #     View movie page function that returns the movie details page and its data
-# #     '''
-# #     movie = get_movie(id)
-# #     title = f'{movie.title}'
-# #     reviews = Review.get_reviews(movie.id)
-# #
-# #     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-# #
-# #
-# # @main.route('/search/<movie_name>')
-# # def search(movie_name):
-# #     '''
-# #     View function to display the search results
-# #     '''
-# #     movie_name_list = movie_name.split(" ")
-# #     movie_name_format = "+".join(movie_name_list)
-# #     searched_movies = search_movie(movie_name_format)
-# #     title = f'search results for {movie_name}'
-# #     return render_template('search.html',movies = searched_movies)
-# #
-# #
-# # @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-# # @login_required
-# # def new_review(id):
-# #     form = ReviewForm()
-# #     movie = get_movie(id)
-# #
-# #     if form.validate_on_submit():
-# #         title = form.title.data
-# #         review = form.review.data
-# #         new_review = Review(movie.id,title,movie.poster,review)
-# #         new_review.save_review()
-# #         return redirect(url_for('.movie',id = movie.id ))
-# #
-# #     title = f'{movie.title} review'
-# #     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+from flask import render_template, request, redirect, url_for, abort, flash
+from . import main
+from flask_login import login_required, current_user
+from ..models import Pitch, User, Comment, Upvote, Downvote
+from .forms import PitchForm, CommentForm, UpvoteForm
+from flask.views import View, MethodView
+from .. import db
+import markdown2
+
+
+# Views
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    pitch = Pitch.query.filter_by().first()
+    title = 'Home'
+    pickuplines = Pitch.query.filter_by(category="pickuplines")
+    interviewpitch = Pitch.query.filter_by(category="interviewpitch")
+    promotionpitch = Pitch.query.filter_by(category="promotionpitch")
+    productpitch = Pitch.query.filter_by(category="productpitch")
+
+    upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
+
+    return render_template('home.html', title=title, pitch=pitch, pickuplines=pickuplines,
+                           interviewpitch=interviewpitch, promotionpitch=promotionpitch, productpitch=productpitch,
+                           upvotes=upvotes)
+
+
+@main.route('/pitches/new/', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    my_upvotes = Upvote.query.filter_by(pitch_id=Pitch.id)
+    if form.validate_on_submit():
+        description = form.description.data
+        title = form.title.data
+        owner_id = current_user
+        category = form.category.data
+        print(current_user._get_current_object().id)
+        new_pitch = Pitch(owner_id=current_user._get_current_object().id, title=title, description=description,
+                          category=category)
+        db.session.add(new_pitch)
+        db.session.commit()
+
+        return redirect(url_for('main.index'))
+    return render_template('pitches.html', form=form)
+
+
+@main.route('/comment/new/<int:pitch_id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    if form.validate_on_submit():
+        description = form.description.data
+
+        new_comment = Comment(description=description, user_id=current_user._get_current_object().id, pitch_id=pitch_id)
+        db.session.add(new_comment)
+        db.session.commit()
+
+        return redirect(url_for('.new_comment', pitch_id=pitch_id))
+
+    all_comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+    return render_template('comments.html', form=form, comment=all_comments, pitch=pitch)
+
+
+@main.route('/pitch/upvote/<int:pitch_id>/upvote', methods=['GET', 'POST'])
+@login_required
+def upvote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+    pitch_upvotes = Upvote.query.filter_by(pitch_id=pitch_id)
+
+    if Upvote.query.filter(Upvote.user_id == user.id, Upvote.pitch_id == pitch_id).first():
+        return redirect(url_for('main.index'))
+
+    new_upvote = Upvote(pitch_id=pitch_id, user=current_user)
+    new_upvote.save_upvotes()
+    return redirect(url_for('main.index'))
+
+
+#    new_upvote = Upvote(user=current_user, pitch=pitch, vote_number=1)
+#    new_vote.save_vote()
+# return redirect(url_for('main.index'))
+
+
+@main.route('/pitch/downvote/<int:pitch_id>/downvote', methods=['GET', 'POST'])
+@login_required
+def downvote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+    pitch_downvotes = Downvote.query.filter_by(pitch_id=pitch_id)
+
+    if Downvote.query.filter(Downvote.user_id == user.id, Downvote.pitch_id == pitch_id).first():
+        return redirect(url_for('main.index'))
+
+    new_downvote = Downvote(pitch_id=pitch_id, user=current_user)
+    new_downvote.save_downvotes()
+    return redirect(url_for('main.index'))
+
+
+
+
+
