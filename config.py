695102058d4d9252e5c7dbefb1997f8ab9d4d6ec
nimomaina <nimomaina.nm@gmail.com>
2019-02-12 12:00:41 +0300

    Add email configurations

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wairimu:touce@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'powerfulsecretkey'
+    SECRET_KEY = 'nimokey'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-
+export MAIL_USERNAME=nimomaina.nm@gmail.com
+export MAIL_PASSWORD=home2464$
 python3.6 manage.py server
